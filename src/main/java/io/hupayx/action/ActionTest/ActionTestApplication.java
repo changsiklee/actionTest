a4888c44e023da14e5024d0cd57c6d0429cc6f14
이창식 <cs.lee@hupayx.com>
2022-04-21 13:59:34 +0900

    First Release

---
+++ src/main/java/io/hupayx/action/ActionTest/ActionTestApplication.java
@@ -0,0 +1,14 @@
+package io.hupayx.action.ActionTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		SpringApplication.run(ActionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hupayx/action/ActionTest/ActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hupayx.action.ActionTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
