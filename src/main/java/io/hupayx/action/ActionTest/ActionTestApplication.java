7721bcf634115301613d2556ca80fb8d594fbb6b
이창식 <cs.lee@hupayx.com>
2022-05-13 11:12:49 +0900

    modify the main class

--- src/main/java/io/hupayx/action/ActionTest/ActionTestApplication.java
+++ src/main/java/io/hupayx/action/ActionTest/ActionTestApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ActionTestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("start");
-		System.out.println("ActionTestApplication main method execute.");
 		SpringApplication.run(ActionTestApplication.class, args);
 	}
 
