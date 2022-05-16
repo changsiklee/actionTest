a99699f43d91657f1234d2c3c754622cca6c3421
이창식 <cs.lee@hupayx.com>
2022-05-16 10:45:10 +0900

    modify the deploy.sh file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,30 +2,26 @@
 
 REPOSITORY=/home/ec2-user/apps/deploy
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> Determine which application pid is currently running."
 
 CURRENT_PID=$(pgrep -fl Action | grep java | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "the currently running application pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "No applications are currently running, so do not kill."
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> Deploy a new application"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
 chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
+echo "> $JAR_NAME execution"
 
 nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
