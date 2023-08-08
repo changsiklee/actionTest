4d22494d9a2065a502da980fc24680189350ebed
changsiklee <97146684+changsiklee@users.noreply.github.com>
2023-08-09 01:23:53 +0900

    Update ActionTestApplication.java2

--- src/main/java/io/hupayx/action/ActionTest/ActionTestApplication.java
+++ src/main/java/io/hupayx/action/ActionTest/ActionTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ActionTestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("ActionTestApplication");
+		System.out.println("ActionTestAppdlication");
 		SpringApplication.run(ActionTestApplication.class, args);
 	}
 
